67e2bc4536a3c157c0bbbf3f7ae141a2508a417a
Josh M <joshmeth30@gmail.com>
2017-08-13 22:36:27 -0400

    gui stuff and directory stuff

--- pyqt5/gui.py
+++ pyqt5/gui.py
@@ -1,8 +1,12 @@
 # RECORD BUTTON GUI
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget, QPushButton
+from PyQt5.QtWidgets import (QApplication, QComboBox, QDialog,
+        QDialogButtonBox, QFormLayout, QGridLayout, QGroupBox, QHBoxLayout,
+        QLabel, QLineEdit, QMenu, QMenuBar, QPushButton, QSpinBox, QTextEdit,
+        QVBoxLayout, QWidget)
 from PyQt5.QtGui import QIcon
 from PyQt5.QtCore import pyqtSlot
+
 
 import pyaudio
 import wave
@@ -10,16 +14,22 @@ import wave
 
 
 class App(QWidget):
- 
     def __init__(self):
         super().__init__()
-        self.title = 'PyQt5 button - pythonspot.com'
+        self.title = 'QPushButton'
         self.left = 10
         self.top = 10
         self.width = 320
         self.height = 200
         self.initUI()
- 
+        # create form with number spinner
+        # self.spinner()
+        # mainLayout = QVBoxLayout()
+        # mainLayout.addWidget(self.formGroupBox)
+        # self.setLayout(mainLayout)
+        
+        
+    
     def initUI(self):
         self.setWindowTitle(self.title)
         self.setGeometry(self.left, self.top, self.width, self.height)
@@ -28,9 +38,24 @@ class App(QWidget):
         button.setToolTip('This is an example button')
         button.move(100,70) 
         button.clicked.connect(self.recSound)
- 
+        button.setStyleSheet("background-color: #cc3a33; border: none; font-weight:bold;") 
+        button.resize(100,100)
         self.show()
- 
+
+    # def spinner(self):
+    #     self.formGroupBox = QGroupBox("")
+    #     layout = QFormLayout()
+    #     # layout.addRow(QLabel("Name:"), QLineEdit())
+    #     # layout.addRow(QLabel("Country:"), QComboBox())
+    #     spinner = QSpinBox()
+    #     spinner.setValue(4)
+    #     # spinner.setStyleSheet("QSpinBox { border: 3px solid red; border-radius: 5px; background-color: yellow; min-height: 150px; min-width: 150px; }")
+    #     # spinner.setStyleSheet("QSpinBox { border: 3px inset grey; width:150px;} ")
+    #     # spinner.setStyleSheet("QSpinBox::down-button { subcontrol-position: right; width: 40px; height: 35px;}")
+    #     # spinner.setStyleSheet("QSpinBox::up-button { subcontrol-position: left; width: 40px; height: 35px;}")
+    #     layout.addRow(QLabel("Time:"), spinner)
+    #     self.formGroupBox.setLayout(layout)
+
     @pyqtSlot()
     def on_click(self):
         print('PyQt5 button click')
@@ -43,30 +68,22 @@ class App(QWidget):
         RATE = 44100 #sample rate
         RECORD_SECONDS = 5
         WAVE_OUTPUT_FILENAME = "rp3audio/rp3output.wav"
-
         p = pyaudio.PyAudio()
-
         stream = p.open(format=FORMAT,
                         channels=CHANNELS,
                         rate=RATE,
                         input=True,
                         frames_per_buffer=CHUNK) #buffer
-
         print("* recording")
-
         frames = []
-
         for i in range(0, int(RATE / CHUNK * RECORD_SECONDS)):
 
             data = stream.read(CHUNK)
             frames.append(data) # 2 bytes(16 bits) per channel
-
         print("* done recording")
-
         stream.stop_stream()
         stream.close()
         p.terminate()
-
         wf = wave.open(WAVE_OUTPUT_FILENAME, 'wb')
         wf.setnchannels(CHANNELS)
         wf.setsampwidth(p.get_sample_size(FORMAT))
@@ -74,6 +91,8 @@ class App(QWidget):
         wf.writeframes(b''.join(frames))
         wf.close()
         return;
+    
+
  
 if __name__ == '__main__':
     app = QApplication(sys.argv)
