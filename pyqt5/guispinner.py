c0b76779c227267724fc2445d64eb39edaf1cf86
Josh M <joshmeth30@gmail.com>
2017-08-18 00:34:33 -0400

    guispinner works

--- pyqt5/guispinner.py
+++ pyqt5/guispinner.py
@@ -3,6 +3,7 @@ from PyQt5.QtWidgets import (QApplication, QComboBox, QDialog,
         QLabel, QLineEdit, QMenu, QMenuBar, QPushButton, QSpinBox, QTextEdit,
         QVBoxLayout, QInputDialog)
 from PyQt5.QtCore import pyqtSlot, pyqtSignal
+from functools import partial
 import sys
 import pyaudio
 import wave
@@ -10,7 +11,7 @@ import wave
 class Dialog(QDialog):
     NumGridRows = 3
     NumButtons = 4
- 
+
     def __init__(self):
         super(Dialog, self).__init__()
         self.createFormGroupBox()
@@ -19,7 +20,7 @@ class Dialog(QDialog):
         self.width = 320
         self.height = 200
 
-    
+        self.newTime = 10
         mainLayout = QVBoxLayout()
         mainLayout.addWidget(self.formGroupBox)
         # mainLayout.addWidget(buttonBox)
@@ -31,16 +32,13 @@ class Dialog(QDialog):
         layout = QFormLayout()
         # SPINNER STUFF
         spinner = QSpinBox()
-        
         spinner.setStyleSheet("QSpinBox { border: 3px inset grey; width:100px; height:50px;} QSpinBox::up-button { subcontrol-position: left; width: 40px; height: 35px;}  QSpinBox::down-button { subcontrol-position: right; width: 40px; height: 35px;}")
         spinner.setValue(4)
         print("Seconds:", spinner.value())
-        # @pyqtSignal()
+
         spinner.valueChanged.connect(self.secondsChange)
-        # if spinner.valueChanged():
-        #     print("spinner.value()")
-        # self.spinner.valueChanged.connect(self.print(value()))
         layout.addRow(QLabel("Sec:"), spinner)        
+        #Need to get the value of the spinner to recSound
         # BUTTON STUFF
         button = QPushButton('Record')
         button.clicked.connect(self.recSound)
@@ -48,19 +46,19 @@ class Dialog(QDialog):
         layout.addRow(button)
         self.formGroupBox.setLayout(layout)
 
-    @pyqtSlot(int)
+    # @pyqtSlot(int)
     def secondsChange(self, value):
         print("Seconds:", value)
+        self.newTime = value
 
-
-    @pyqtSlot()
+    # @pyqtSlot()
     def recSound(self):
         # This records an audio file
         CHUNK = 512
         FORMAT = pyaudio.paInt16 #paInt8
         CHANNELS = 1
         RATE = 44100 #sample rate
-        RECORD_SECONDS = 5
+        RECORD_SECONDS = self.newTime #this is where the variable for seconds should live from the spinbox
         WAVE_OUTPUT_FILENAME = "rp3audio/rp3output.wav"
         p = pyaudio.PyAudio()
         stream = p.open(format=FORMAT,
@@ -68,7 +66,7 @@ class Dialog(QDialog):
                         rate=RATE,
                         input=True,
                         frames_per_buffer=CHUNK) #buffer
-        print("* recording")
+        print("* Recording for ", self.newTime)
         frames = []
         for i in range(0, int(RATE / CHUNK * RECORD_SECONDS)):
 
